6e0952880d5ac6e4ff1f5524d9db057265f2914f
minjung <suk9394ss@gmail.com>
2023-02-07 17:46:41 +0900

    django change pw

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from account.views import hello_world, AccountCreateView
+from account.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = "account"
 
@@ -10,6 +10,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="account/login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 ]
+
 
 """127.0.0.0:8000/account/helloworld == account(app_name):hello_world(url name)"""
--- account/views.py
+++ account/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from account.forms import AccountUpdateForm
 from account.models import HelloWorld
 
 
@@ -29,3 +30,14 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('account:hello_world')
     template_name = 'account/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'account/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('account:hello_world')
+    template_name = 'account/update.html'
